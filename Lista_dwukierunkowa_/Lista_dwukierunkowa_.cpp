2c6e4930b506212e90aaba2ea84e26cae964426c
OG-AdiG <adriangargula@wp.pl>
2024-10-14 22:23:42 +0200

    Dodanie plików i utworzenie klas

---
+++ Lista_dwukierunkowa_/Lista_dwukierunkowa_.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Lista {
+
+private:
+
+	int pozycja;
+	int data;
+	Lista* next;
+	Lista* prev;
+
+public:
+
+	Lista(int a = 0) :data(a), prev(nullptr), next(nullptr) {};
+	~Lista(void) {};
+
+};
