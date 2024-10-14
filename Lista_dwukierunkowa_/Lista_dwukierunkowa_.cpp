753e51de0fc5ecaee034f60715fa0837f8d22dcc
OG-AdiG <adriangargula@wp.pl>
2024-10-14 22:32:58 +0200

    Dodanie metod

--- Lista_dwukierunkowa_/Lista_dwukierunkowa_.cpp
+++ Lista_dwukierunkowa_/Lista_dwukierunkowa_.cpp
@@ -16,4 +16,66 @@ public:
 	Lista(int a = 0) :data(a), prev(nullptr), next(nullptr) {};
 	~Lista(void) {};
 
+	void DodajNaPocz();
+	void DodajNaKon();
+	void DodajNaID();
+	void UsunZPocz();
+	void UsunZKonc();
+	void UsunZID();
+	void WyswietlListe();
+	void WyswietlOdwListe();
+	void WyswietlNastElem();
+	void WyswietlPoprzElem();
+	void WyczyscListe();
+					
 };
+
+void Lista::DodajNaPocz()
+{
+}
+
+void Lista::DodajNaKon()
+{
+}
+
+void Lista::DodajNaID()
+{
+}
+
+void Lista::UsunZPocz()
+{
+}
+
+void Lista::UsunZKonc()
+{
+}
+
+void Lista::UsunZID()
+{
+}
+
+void Lista::WyswietlListe()
+{
+}
+
+void Lista::WyswietlOdwListe()
+{
+}
+
+void Lista::WyswietlNastElem()
+{
+}
+
+void Lista::WyswietlPoprzElem()
+{
+}
+
+void Lista::WyczyscListe()
+{
+}
+
+
+int main() {
+
+
+}
